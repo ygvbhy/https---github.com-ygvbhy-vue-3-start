4be96814bd6290566005e5c75fd1a5577b3ba5e2
ygvbhy <ygvbhy@naver.com>
2023-12-21 22:30:40 +0900

    Vue-cli 로 설치 및 기초 작업

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ message }}</div>
+  <button @click="showAlert">경고</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hi",
+    };
+  },
+  methods: {
+    showAlert() {
+      alert("hello");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
