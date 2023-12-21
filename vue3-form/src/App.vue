5ea5a266712f004375499890a9a8610b3ff9d2fa
ygvbhy <ygvbhy@naver.com>
2023-12-21 22:55:20 +0900

    프로젝트 생성 및 간단한 로그인 폼 UI 구성

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userId">ID:</label>
+      <input type="text" id="yserID" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW:</label>
+      <input type="password" id="password" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
