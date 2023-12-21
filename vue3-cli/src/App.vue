28daf5b61c7f2510825e3b6bffaed1634cb830f2
ygvbhy <ygvbhy@naver.com>
2023-12-21 22:38:52 +0900

    Vue 컴포넌트 등록 방법 및 명명 규칙

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,10 +1,21 @@
 <template>
+  <AppHeader />
   <div>{{ message }}</div>
   <button @click="showAlert">경고</button>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
 export default {
+  components: {
+    // 축약 과정
+    // 'app-header': AppHeader => 케밥 케이스
+    // 'AppHeader': AppHeader => 카멜 케이스
+    // AppHeader: AppHeader => 파스칼 케이스
+    AppHeader,
+
+    // 번외 -> app_header => 스네이크케이스
+  },
   data() {
     return {
       message: "hi",
